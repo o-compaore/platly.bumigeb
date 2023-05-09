74cfd2e91dc6e59232aa07abd73dcafe6b8b0a4e
oumar-farouk <compaore7435@gmail.com>
2023-05-09 14:09:31 +0000

    Ajustement pour le deploiement sur github

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,20 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        app: './App/App.js',
-        vendor: ["react","react-dom"]
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, '../public')
-    },
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader?cacheDirectory=true',
-            }
-        }]
-    }
-};
+  entry: './index',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
